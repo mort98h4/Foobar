b7edb9e9d9301855ab22836fcffc46f09ce88e54
Morten Gross <mg@vuffeli.dk>
2021-05-18 11:44:51 +0200

    Basic navigation is working with routering

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,28 @@ import logo from './logo.svg'
 import { Router, Link } from "@reach/router"
 
 function App() {
-  let Home = () => <div className="App"><h1>FooBar</h1>
+  let Home = () => <div><h1>FooBar</h1>
   <h2>3. semester eksamen</h2></div>
   let Dash = () => <div>Dash</div>
 
   return (
-    <Router>
-    <Home path="/" />
-    <Dash path="dashboard" />
-  </Router>
+    <div className="App">
+      <Nav></Nav>
+      <Router>
+        <Home path="/" />
+        <Dash path="dashboard" />
+      </Router>
+    </div>
   )
 }
 
 export default App
+
+function Nav() {
+  return(
+    <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="dashboard">Dashboard</Link>
+    </nav>
+  )
+}
