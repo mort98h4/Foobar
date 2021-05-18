36df397ee0a1b7b84b37f79078fa272f44fd38d0
Morten Gross <mg@vuffeli.dk>
2021-05-18 13:22:05 +0200

    Bootstrap added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,8 +2,12 @@ import React, {useState} from "react";
 
 export default function Dashboard() {
     return(
-        <div>
-            <h1>Dashboard</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1>Dashboard</h1>
+                </div>
+            </div>
         </div>
     )
 }
