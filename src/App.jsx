8e989de0a4e5fdcba4ff1a0f261bc9390d0c7cd6
Morten Gross <mg@vuffeli.dk>
2021-05-18 11:27:47 +0200

    added reach/router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import logo from './logo.svg'
+import { Router, Link } from "@reach/router"
 
 function App() {
+  let Home = () => <div className="App"><h1>FooBar</h1>
+  <h2>3. semester eksamen</h2></div>
+  let Dash = () => <div>Dash</div>
+
   return (
-      <main className="App">
-          <h1>FooBar</h1>
-          <h2>3. semester eksamen</h2>
-      </main>
+    <Router>
+    <Home path="/" />
+    <Dash path="dashboard" />
+  </Router>
   )
 }
 
