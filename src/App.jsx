e6c1b38e61a392f19dabfc0422156a138833ff33
Morten Gross <mg@vuffeli.dk>
2021-05-18 11:03:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+
+function App() {
+  return (
+      <main className="App">
+          <h1>FooBar</h1>
+          <h2>3. semester eksamen</h2>
+      </main>
+  )
+}
+
+export default App
