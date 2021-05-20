8f196d8a56e1bffbd646c7beea0e2ea764f665cf
Siw <siwx0310@stud.kea.dk>
2021-05-20 14:50:00 +0200

    simple filter on types

--- src/pages/Beers.jsx
+++ src/pages/Beers.jsx
@@ -4,11 +4,34 @@ import Product from "../components/Product";
 export default function Beers(props) {
     // console.log(props);
     const productComponents = props.products.map((item) => <Product key={item.name} {...item} />)
+
+    const [sortKey, setSortKey] = useState("name");
+    const [sortDirection, setSortDirection] = useState("asc");
+
+    props.products.sort(compare);
+    function compare(a, b) {
+      if (a[sortKey] > b[sortKey]) {
+        return sortDirection === "asc" ? 1 : -1;
+      } else {
+        return sortDirection === "asc" ? -1 : 1;
+      }
+    }
+  
+    function toggleSort(key) {
+      if (key === sortKey) {
+        setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+      }
+      setSortKey(key);
+    }
+
     return(
         <main className="container">
-            <header className="row">
-                <h1>Beers</h1>
-            </header>
+           <div>
+               <button className="btn btn-primary" onClick={() => toggleSort("price")}>Filter Kind</button>
+               <button className="btn btn-primary" onClick={() => toggleSort("alc")}>Filter Alc</button>
+               <button className="btn btn-primary" onClick={() => toggleSort("category")}>Filter Category</button>
+               <button className="btn btn-primary" onClick={() => toggleSort("popularity")}>Filter Popularity</button>
+           </div>
             {productComponents}
         </main>
     )
