e006231fe1b467c9987d2011735f3e55fe59d247
Siw <siwx0310@stud.kea.dk>
2021-05-20 10:31:27 +0200

    navbar w.bootstrap, without styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,18 +3,15 @@ import { Link } from "@reach/router"
 
 export default function Nav() {
     return(
-        <div className="container">
-        <nav className="row">
-            <div className="col">
-            <Link  to="/">Dashboard</Link> |{" "}
-            <Link  to="beers">Beers </Link> | {" "}
-            </div>
-            <span className="col">Hardcodet you are number <span>"something"</span> in the line</span> | {" "}
-            <div className="col">
-            <Link  to="cart">Cart</Link> <span>0</span> | {" "}
-            <Link  to="ratings">Ratings</Link>
-            </div>
+        
+        <nav className="navbar navbar-light bg-light bottom-0 start-0 position-fixed">
+            <Link className="navbar-brand" to="/">Dashboard</Link> 
+            <Link className="navbar-brand" to="beers">Beers </Link> 
+            <h4 className="navbar-text">You are number <span>"Hardcodet"</span> in the line</h4> 
+            <Link className="navbar-brand" to="cart">Cart<span>0</span></Link>  
+            <Link className="navbar-brand" to="ratings">Ratings</Link>
+         
         </nav>
-        </div>
+
     )
 }
