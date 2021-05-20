6ab83fe8a67e85116911b4316d56c92ea9d0ded7
Siw <siwx0310@stud.kea.dk>
2021-05-20 09:52:02 +0200

    changed nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,18 @@ import { Link } from "@reach/router"
 
 export default function Nav() {
     return(
-        <nav>
-            <Link to="/">Dashboard</Link> |{" "}
-            <Link to="beers">Beers</Link> |{" "}
-            <Link to="cart">Cart</Link> |{" "}
-            <Link to="ratings">Ratings</Link>
+        <div className="container">
+        <nav className="row">
+            <div className="col">
+            <Link  to="/">Dashboard</Link> |{" "}
+            <Link  to="beers">Beers </Link> | {" "}
+            </div>
+            <span className="col">Hardcodet you are number <span>"something"</span> in the line</span> | {" "}
+            <div className="col">
+            <Link  to="cart">Cart</Link> <span>0</span> | {" "}
+            <Link  to="ratings">Ratings</Link>
+            </div>
         </nav>
+        </div>
     )
 }
