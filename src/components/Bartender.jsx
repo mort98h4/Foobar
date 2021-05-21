84d36ca5315584284230446bb8203af56fdb9ac6
Morten Gross <mg@vuffeli.dk>
2021-05-21 14:36:51 +0200

    removed console.log

--- src/components/Bartender.jsx
+++ src/components/Bartender.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Bartender(props) {
-    console.log(props);
     return (
         <div className="col-4">
             <img src="" alt=""></img>
