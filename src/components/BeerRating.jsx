5cb3420cd14a5c418c8e8b193884e27ebb02582f
Morten Gross <mg@vuffeli.dk>
2021-05-21 11:58:29 +0200

    BeerRating component is working

---
+++ src/components/BeerRating.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+
+export default function BeerRating(props) {
+    return(
+        <div className="row">
+            <div className="col-6">
+                <h3>{props.name}</h3>
+            </div>
+            <div className="col-6 d-flex justify-content-end">
+                <div className="star" data-id={props.id} data-rating="1" onClick={(e) => props.clickStarHandler(props)}></div>
+                <div className="star" data-id={props.id} data-rating="2" onClick={(e) => props.clickStarHandler(props)}></div>
+                <div className="star" data-id={props.id} data-rating="3" onClick={(e) => props.clickStarHandler(props)}></div>
+                <div className="star" data-id={props.id} data-rating="4" onClick={(e) => props.clickStarHandler(props)}></div>
+                <div className="star" data-id={props.id} data-rating="5" onClick={(e) => props.clickStarHandler(props)}></div>
+            </div>
+        </div>
+    )
+}
