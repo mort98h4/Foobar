a847920c447a2bbe0efeeaed0a658399395c5843
Siw <siwx0310@stud.kea.dk>
2021-05-23 16:07:20 +0200

    initial commit

---
+++ src/components/Pay.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Pay() {
+    return (
+        <div className="mb-3">
+            <label className="exampleFormControlInput1">
+                Email: 
+            </label>
+                <input type="email" className="form-control" id="exampleFormControlInput1"></input>
+        </div>
+        ); 
+}
