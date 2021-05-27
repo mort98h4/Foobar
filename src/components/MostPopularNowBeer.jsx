3e908651803f51b4bebc12d26708e0afe05e7471
Morten Gross <mg@vuffeli.dk>
2021-05-27 11:33:36 +0200

    Images has alt text, and edited some layout

--- src/components/MostPopularNowBeer.jsx
+++ src/components/MostPopularNowBeer.jsx
@@ -5,13 +5,13 @@ import cleanImageName from "../helpers/cleanImageName.js";
 export default function MostPopularNowBeer(props) {
     const index = props.topThree.findIndex(item=>item.name === props.name);
     const imagePath = cleanImageName(props.name);
-    console.log(imagePath);
+    const imageAlt = `Label of ${props.name}`;
     return(
         <div className="col-4">
-            <div className="row">
-                <div className="col-12 d-flex justiy-content-center">
+            <div className="row justify-content-center">
+                <div className="col-6">
                     <LazyLoad height={200} once={true} offset={100}>
-                        <img src={imagePath} alt="" width="100%"></img>
+                        <img src={imagePath} alt={imageAlt}></img>
                     </LazyLoad>
                 </div>
                 <div className="col-12 text-center">
