5159251351a2d969e3c6496600e2e685e1e0980b
Morten Gross <mg@vuffeli.dk>
2021-05-27 11:24:16 +0200

    Images is now loaded

--- src/components/MostPopularNowBeer.jsx
+++ src/components/MostPopularNowBeer.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import LazyLoad from 'react-lazyload';
+import cleanImageName from "../helpers/cleanImageName.js";
 
 export default function MostPopularNowBeer(props) {
     const index = props.topThree.findIndex(item=>item.name === props.name);
+    const imagePath = cleanImageName(props.name);
+    console.log(imagePath);
     return(
         <div className="col-4">
             <div className="row">
                 <div className="col-12 d-flex justiy-content-center">
-                    <img src="" alt=""></img>
+                    <LazyLoad height={200} once={true} offset={100}>
+                        <img src={imagePath} alt="" width="100%"></img>
+                    </LazyLoad>
                 </div>
                 <div className="col-12 text-center">
                     <p><span>#{index+1} </span>{props.name}</p>
---
+++ src/helpers/cleanImageName.js
@@ -0,0 +1,5 @@
+export default function cleanImageName(beerName) {
+    const newBeerName = beerName.toLowerCase().replaceAll(" ", "");
+    const beerNamePath = `.././${newBeerName}.png`;
+    return beerNamePath;
+}
