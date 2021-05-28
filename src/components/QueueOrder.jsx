a42cef242d6ed0b1f5175572c16b3875f4214cc0
Morten Gross <mg@vuffeli.dk>
2021-05-28 20:01:47 +0200

    Generates a random name for orders not posted by the user

--- src/components/QueueOrder.jsx
+++ src/components/QueueOrder.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 export default function QueueOrder(props) {
+    console.log(props);
+    console.log(props.userOrder[0].id);
+
+    const randomNames = ["Simone", "Rie", "Camilla", "Kristine", "Marie", "Anders", "Søren", "Christian", "Phillip", "Thomas"];
+    const namesLen = randomNames.length;
+    const randomNumber = getRandomNumber(namesLen);
+    const randomName = getRandomName(randomNames, randomNumber);
+
     return (
         <div className="queueOrder row justify-content-between">
             <div className="col">
-                <p>Morten</p>
+                <p>{props.id === props.userOrder[0].id ? props.userOrder[0].name : randomName}</p>
             </div>
             <div className="col text-end">
                 <p>No. </p>
@@ -12,4 +20,12 @@ export default function QueueOrder(props) {
             </div>
         </div>
     )
+}
+
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getRandomName(names, num) {
+    return names[num];
 }
