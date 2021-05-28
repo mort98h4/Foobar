1bb6bcada1632c040e94860eb49ad7093ed8dda4
Morten Gross <mg@vuffeli.dk>
2021-05-28 14:49:11 +0200

    Added stars and the logic behind to beerOnTap

--- src/components/BeerOnTap.jsx
+++ src/components/BeerOnTap.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import roundRatingAvg from "../helpers/roundRatingAvg.jsx";
 
 export default function BeerOnTap(props) {
+    const ratingAVGRound = roundRatingAvg(props.ratingAVG);
     return (
         <div className="beerOnTap row justify-content-between">
             <div className="col-6">
                 <p>{props.name}</p>
-                <span>{props.ratingAVG}</span>
+                {ratingAVGRound}
             </div>
             <div className="col-6 text-end">
                 <p className="price"><span>49</span>,-</p>
---
+++ src/helpers/roundRatingAvg.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Star from "../components/Star";
+
+export default function roundRatingAvg(rating) {
+    const round = Math.round(rating);
+    console.log(round);
+    if (round === 1) {
+        return (
+            <div className="row mb-1">
+                <div className="col-12 d-flex">
+                    <Star></Star>
+                </div>
+            </div>
+        )
+    } else if (round === 2) {
+        return (
+            <div className="row mb-1">
+                <div className="col-12 d-flex">
+                    <Star></Star>
+                    <Star></Star>
+                </div>
+            </div>
+        )
+    } else if (round === 3) {
+        return (
+            <div className="row mb-1">
+                <div className="col-12 d-flex">
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                </div>
+            </div>
+        )
+    } else if (round === 4) {
+        return (
+            <div className="row mb-1">
+                <div className="col-12 d-flex">
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div className="row mb-1">
+                <div className="col-12 d-flex">
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                    <Star></Star>
+                </div>
+            </div>
+        )
+    }
+}
