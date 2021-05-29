86aee351607a749d61141427847ec0b67368ea3e
Siw <siwx0310@stud.kea.dk>
2021-05-29 14:51:06 +0200

    dropdown for extra product info

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,6 +6,65 @@ export default function Product(props) {
   const [amount, setAmount] = useState(0);
   let onTap = false;
 
+  /*
+  const accordingHeaderH2 = document.querySelector("#accordionExample h2");
+  const accordingHeaderBtn = document.querySelector("#accordionExample h2 button")
+
+  if (accordingHeaderH2 != null) {
+    if (props.name == "El Hefe") {
+      accordingHeaderH2.setAttribute("id", "headingOne");
+      accordingHeaderBtn.setAttribute("data-bs-target", "collapseOne");
+      console.log(props.name);
+      console.log("1");
+    } else if (props.name == "Fairy Tale Ale") {
+      accordingHeaderH2.setAttribute("id", "headingTwo");
+      accordingHeaderBtn.setAttribute("data-bs-target", "collapseTwo");
+      console.log(props.name);
+      console.log("2");
+    } else if (props.name == "GitHop") {
+      accordingHeaderH2.setAttribute("id", "headingThree");
+      accordingHeaderBtn.setAttribute("data-bs-target", "collapseThree");
+      console.log(props.name);
+      console.log("3");
+    } else if (props.name == "Hollaback Lager") {
+      accordingHeaderH2.setAttribute("id", "headingFour");
+      accordingHeaderBtn.setAttribute("data-bs-target", "collapseFour");
+      console.log(props.name);
+      console.log("4");
+    } else if (props.name == "Hoppily Ever After") {
+      accordingHeaderH2.setAttribute("id", "headingFive");
+      accordingHeaderBtn.setAttribute("data-bs-target", "collapseFive");
+      console.log(props.name);
+      console.log("5");
+    } else if (props.name == "Mowintime") {
+      accordingHeaderH2.setAttribute("id", "headingSix");
+      accordingHeaderBtn.setAttribute("data-bs-target", "collapseSix");
+      console.log(props.name);
+      console.log("6");
+    } else if (props.name == "Row 26") {
+      accordingHeaderH2.setAttribute("id", "headingSeven");
+      accordingHeaderBtn.setAttribute("data-bs-target", "collapseSeven");
+      console.log(props.name);
+      console.log("7");
+    } else if (props.name == "Ruined Childhood") {
+      accordingHeaderH2.setAttribute("id", "headingEight");
+      accordingHeaderBtn.setAttribute("data-bs-target", "collapseEight");
+      console.log(props.name);
+      console.log("8");
+    } else if (props.name == "Sleighride") {
+      accordingHeaderH2.setAttribute("id", "headingNine");
+      accordingHeaderBtn.setAttribute("data-bs-target", "collapseNine");
+      console.log(props.name);
+      console.log("9");
+    } else if (props.name == "Steampunk") {
+      accordingHeaderH2.setAttribute("id", "headingTen");
+      accordingHeaderBtn.setAttribute("data-bs-target", "collapseTen");
+      console.log(props.name);
+      console.log("10");
+    }
+  }
+  */
+
   function clickedPlus() {
     setAmount((prevState) => {
       return prevState + 1;
@@ -113,14 +172,33 @@ export default function Product(props) {
           </div>
         </div>
       </div>
-      <div className="row justify-content-center">
-        <div className="col">
-          <button className="btn btn-primary w-100" type="button">
-            View more
-          </button>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="false"
+              aria-controls="collapseOne"
+            >
+              View more
+            </button>
+          </h2>
+          <ul className="dropdown-menu w-100"></ul>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body">
+              <Carousel slides={slides} autoplay={false} interval={1000} />
+            </div>
+          </div>
         </div>
       </div>
-      <Carousel slides={slides} autoplay={false} interval={1000} />
     </article>
   );
 }
