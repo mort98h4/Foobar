378c2aded6420043284b42c0b0f70bedea4a1ecf
Siw <siwx0310@stud.kea.dk>
2021-06-01 16:00:40 +0200

    small changes

--- src/components/Foobar.jsx
+++ src/components/Foobar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Foobar(props) {
   return (
     <div className="row text-center pt-3">
-        <h1>{props.bar}</h1>
+      <h1>{props.bar}</h1>
     </div>
   );
 }
