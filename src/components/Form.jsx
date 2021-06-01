768636488848ec973f99b3b84b688ce5e3eec647
Siw <siwx0310@stud.kea.dk>
2021-06-01 15:57:08 +0200

    validating form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -70,30 +70,13 @@ export function formatCreditCardNumber(value) {
     return value;
   }
 
-  const issuer = Payment.fns.cardType(value);
   const clearValue = clearNumber(value);
   let nextValue;
 
-  switch (issuer) {
-    case "amex":
-      nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
-        4,
-        10
-      )} ${clearValue.slice(10, 15)}`;
-      break;
-    case "dinersclub":
-      nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
-        4,
-        10
-      )} ${clearValue.slice(10, 14)}`;
-      break;
-    default:
-      nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
-        4,
-        8
-      )} ${clearValue.slice(8, 12)} ${clearValue.slice(12, 19)}`;
-      break;
-  }
+  nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
+    4,
+    8
+  )} ${clearValue.slice(8, 12)} ${clearValue.slice(12, 19)}`;
 
   return nextValue.trim();
 }
